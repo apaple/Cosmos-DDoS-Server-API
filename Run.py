96ce7ea3747256bb7446ee17db873e7321d29b8f
apaple <76417012+apaple@users.noreply.github.com>
2024-03-30 11:00:31 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,3 +1,6 @@
+
+
+
 import logging
 import time
 import os
@@ -23,38 +26,40 @@ app.register_blueprint(Status)
 
 @app.errorhandler(404)
 def error_404(e):
-  return "Error 404.... oh noo"
+    return "Error 404.... oh noo"
 
-@app.errorhandler(Exception)      
+@app.errorhandler(Exception)          
 def basic_error(e):
-  app.logger.warning(f'Caught exception: {str(e)}')
-  return "Unknown error logged. Contact staff."+str(e)
+    app.logger.warning(f'Caught exception: {str(e)}')
+    return "Unknown error logged. Contact staff."+str(e)
 
 def run_cleanup_task():
     while True:
-        os.system('rm -f proxy.txt && python main.py')
+        os.system('rm -f proxy.txt && python3 main.py')
         time.sleep(600)  # 600 seconds = 10 minutes
+        
 
 if __name__ == "__main__":
 
-  init()
+    init()
 
-  print(Fore.MAGENTA, """
+    print(Fore.MAGENTA, """
 
-  ____                  _  ____ ___ 
- / ___|___ ___ _ __ ___  ___ ___   / \ | _ \_ _|
- | |  / _ \/ __| '_ ` _ \ / _ \/ __|  / _ \ | |_) | | 
- | |__| (_) \__ \ | | | | | (_) \__ \ / ___ \| __/| | 
- \____\___/|___/_| |_| |_|\___/|___/ /_/  \_\_| |___|
+   ____                                   _    ____ ___ 
+  / ___|___  ___ _ __ ___   ___  ___     / \  |  _ \_ _|
+ | |   / _ \/ __| '_ ` _ \ / _ \/ __|   / _ \ | |_) | | 
+ | |__| (_) \__ \ | | | | | (_) \__ \  / ___ \|  __/| | 
+  \____\___/|___/_| |_| |_|\___/|___/ /_/   \_\_|  |___|
 
- [Made by robert streahorn aka robert]
-      [t.me/ambuscade]
-                             
-  """, Fore.RESET)
+  [Made by robert streahorn aka robert]
+            [t.me/ambuscade]
+                                                        
+    """, Fore.RESET)
 
-  if PRODUCTION:
-    print(Fore.GREEN, "API server started.", Fore.RESET)
-    serve(app, host="0.0.0.0", port=5000) # change port here if you want
-  else:
-    Thread(target=app.run, kwargs={"threaded":True}).start()
-    Thread(target=run_cleanup_task).start()
+    if PRODUCTION:
+        print(Fore.GREEN, "API server started.", Fore.RESET)
+        serve(app, host="0.0.0.0", port=5000) # change port here if you want
+    else:
+        Thread(target=app.run, kwargs={"threaded":True}).start()
+        Thread(target=run_cleanup_task).start()
+        
