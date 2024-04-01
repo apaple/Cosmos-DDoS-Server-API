a2cf200803c4e300a844a580e66ff1a2f1558997
apaple <76417012+apaple@users.noreply.github.com>
2024-04-01 06:03:19 -0700

    Add files via upload

---
+++ babi.sh
@@ -0,0 +1,49 @@
+yum install epel-release -y
+apt update -y
+apt install python3 -y
+apt install npm -y
+apt install python3-pip -y
+yum install wget -y
+yum install npm -y
+yum install nodejs -y
+yum install screen -y
+yum install python -y
+yum install python3 -y
+pip install -r requirements.txt
+pip3 install pysocks
+pip3 install httpx
+pip3 install bs4
+pip3 install aiohttp
+py -m pip install -r requirements.txt
+py -m pip install pysocks
+py -m pip install httpx
+py -m pip install bs4
+py -m pip install aiohttp
+npm i chalk
+npm i request
+npm i libquery
+npm i ms
+npm i ping
+npm i random-ua
+npm i user-agents
+npm i sync-request
+npm add sync-request
+npm i cluster
+npm i fs
+npm i http2
+npm i tls
+npm i cryptorandomstring
+npm i crypto
+npm i randomstring
+npm i cluster
+npm i url
+npm i superagent
+npm i superagent-proxy
+npm i axios
+npm i zombie
+npm i cloudscraper
+npm add prompt-sync
+chmod u+x *
+ulimit -n 99999
+sudo screen -dmS ls
+node bot
